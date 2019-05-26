e833801d417baa34490fb1ee081065afb1740cb1
thorhsu <thorshu@gmail.com>
2019-05-26 13:38:55 +0800

    add user admin to manager the posts

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')
+
+admin.site.register(Post, PostAdmin)
+
+
